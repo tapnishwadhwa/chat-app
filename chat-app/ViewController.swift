267a977ff249ea3af482039d7dc5c031c7832428
Tapnish Wadhwa <tapnish@Hotmail.com>
2017-08-17 20:11:22 -0400

    Initial Commit

---
+++ chat-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chat-app
+//
+//  Created by Tapnish on 2017-08-17.
+//  Copyright © 2017 Tapnish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
